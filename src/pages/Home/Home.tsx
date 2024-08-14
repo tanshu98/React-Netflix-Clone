700244ed9fb91983abe4a69ba5b222217e082f79
Tanshu <tanshuallewar98@gmail.com>
2024-08-14 10:33:12 +0530

    Added Home Page UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './HomeStyles';  
 import Navbar from '../../components/Navbar/Navbar';
+import { HomeStyles } from './HomeStyles';
+import heroBanner from '../../assets/hero_banner.jpg'
+import heroTitle from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
-    <>
+    <div style={HomeStyles.mainContainer}>
     <Navbar />
-    </>
+    <div style={HomeStyles.hero}>
+        <img src={heroBanner} alt="banner-img" style={HomeStyles.bannerImg} />
+        <div style={HomeStyles.heroCaption}>
+            <img src={heroTitle} alt="hero-title" />
+            <p>
+                Discovering his ties to secret ancient order, a young man living in the Istanbul embarks on the quest of saving the city from an immoratal enemy.
+            </p>
+        </div>
+    </div>
+    </div>
   )
 }
 
--- src/pages/Home/HomeStyles.ts
+++ src/pages/Home/HomeStyles.ts
@@ -1,3 +1,14 @@
 export const HomeStyles = {
-    
+    mainContainer: {},
+    hero: {
+        position:'relative' as 'relative'
+    },
+    bannerImg:{
+        width:'100%',
+        maskImage: 'linear-gradient(to right, transparent, black 50%)',
+        // WebkitMaskImage: 'linear-gradient(to right, transparent, black 75%)',
+      
+    },
+    heroCaption:{}
+
 }
